c3ddaee2e5d58eacf439c531a14049431f8b561b
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-25 11:34:45 -0500

    Modified layout.tsx file: added the components NavBar and Footer.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import {Footer,NavBar} from '@/atomic/component'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        Navbar
+        <NavBar />
         <main>
           {children}
         </main>
-        Footer 
+        <Footer />
       </body>
     </html>
   )
