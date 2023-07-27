0fda5be2023dcb4a28ad3eff8d2f1c73df78ce91
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-27 00:14:07 -0500

    Modified tailwind.config.js and tsconfig.json.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./atomic/**/*.{js,jsx,ts,tsx}",
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/atomic/**/*.{js,jsx,ts,tsx}",
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
