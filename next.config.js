a734f98270038f00aa6b090fdbfba66c74768e6d
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-29 00:10:36 -0500

    Modified the configuration file.

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
     env: {
         NEXT_PUBLIC_GRAFBASE_API_URL:process.env.NEXT_PUBLIC_GRAFBASE_API_URL,
-        NEXT_PUBLIC_GRABASE_API_KEY:process.env.NEXT_PUBLIC_GRABASE_API_KEY,
+        NEXT_PUBLIC_GRAFBASE_API_KEY:process.env.NEXT_PUBLIC_GRAFBASE_API_KEY,
         GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        NEXTAUTH_URL:process.env.NEXTAUTH_URL
+        NEXTAUTH_URL:process.env.NEXTAUTH_URL,
     },
     images:{
         domains:["lh3.googleusercontent.com"]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/atomic/**/*.{js,jsx,ts,tsx}",
+    "./src/architecture/atomic/**/*.{js,jsx,ts,tsx}",
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
