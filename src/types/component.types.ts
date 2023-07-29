12c7651181d9d0a104fb4591296456e35c23d616
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-29 00:16:46 -0500

    Created the props of the components.

--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -1,7 +1,10 @@
+import { SessionInterface } from "./common.types";
+
 interface FooterColumnProps {
   title: string;
   links: string[];
 }
+
 type Provider = {
   id: string;
   name: string;
@@ -11,5 +14,59 @@ type Provider = {
   signinUrlParams?: Record<string, string> | undefined;
 };
 
+interface ProfileMenuProps {
+  session :  SessionInterface 
+}
+
+interface ProjectFormProps {
+  type:"create"
+  session: SessionInterface
+}
+
+interface PosterProps{
+  id:string,
+  value:any,
+  type:"create"
+  entity:string,
+  disabled?:boolean,
+  handleChange: any,
+  handleBlur:any,
+}
+
+interface FormFieldProps{
+  type?:string;
+  isTextArea?:string;
+  title:string;
+  value:string | undefined;
+  placeholder:string;
+  handleChange:any;
+  handleBlur:any;
+}
+
+interface CustomMenuProps{
+  title:string;
+  value:string | undefined;
+  filters:string[];
+  handleClick:(e:React.MouseEvent<HTMLButtonElement>)=>void
+  handleBlur:any;
+}
+
+interface ButtonProps{
+  title:string;
+  disabled:boolean;
+  children:React.ReactNode
+  handleClick:()=>void;
+}
+
 type Providers = Record<string, Provider>;
-export type { FooterColumnProps, Provider, Providers };
+export type { 
+  FooterColumnProps, 
+  Provider, 
+  Providers, 
+  ProfileMenuProps, 
+  ProjectFormProps, 
+  PosterProps,
+  FormFieldProps,
+  CustomMenuProps,
+  ButtonProps
+};
