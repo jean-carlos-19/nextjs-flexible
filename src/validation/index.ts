ec9e83b42ac5e573b767ae00323d9adbb2c2f8bb
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-08-01 10:26:31 -0500

    Modified the validations of the form.

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -5,15 +5,7 @@ const projectFormValidation = yup.object<ProjectModel>({
     title:yup.string().required("field title is requeried"),
     description:yup.string().required("field description is requeried"),
     image:yup.mixed().required("the field image is requeried"),
-    liveSiteUrl:yup.string().required("website title is requeried")
-    .test("url invalid","website title is invalid",(value:string)=>{
-        try {
-            new URL(value);
-        } catch (error) {
-            console.log(error)
-        }
-        return false;
-    }),
+    liveSiteUrl:yup.string().required("website title is requeried"),
     githubUrl:yup.string().required("field github is requeried"),
     category:yup.string().required("field category is requeried"),
 }) 
